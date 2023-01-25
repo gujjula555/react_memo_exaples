5f43011c313a3697343ff22138b6f0712b9efc9b
ramesh.gujjula <ramesh.gujjula@ril.com>
2023-01-25 23:41:08 +0530

    chan

--- src/component/Child.js
+++ src/component/Child.js
@@ -7,8 +7,11 @@ const Child = (props) => {
     const { childvalue, onIncreaseCount } = props
     console.log("Child component is rendering!!!")
     return (
+        
         <div>
             Child component {childvalue.countChild}
+            <button type="button" onClick={onIncreaseCount}>Child button</button>
+            <button type="button" onClick={onIncreaseCount}>Child button</button>
             <button type="button" onClick={onIncreaseCount}>Child button</button>
         </div>
     )
