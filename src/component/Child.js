0f49245a7cf7ea56d0b699875ad09edb3410c095
ramesh.gujjula <ramesh.gujjula@ril.com>
2023-01-26 11:44:43 +0530

    ff

--- src/component/Child.js
+++ src/component/Child.js
@@ -9,11 +9,7 @@ const Child = (props) => {
     return (
         
         <div>
-            Child component {childvalue.countChild}
-            <button type="button" onClick={onIncreaseCount}>Child button</button>
-            <button type="button" onClick={onIncreaseCount}>Child button</button>
-            <button type="button" onClick={onIncreaseCount}>Child button</button>
-        </div>
+                </div>
     )
 }
 export default memo(Child)
